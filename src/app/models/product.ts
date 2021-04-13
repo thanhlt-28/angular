0fe353d49983137f5b80b572bf32f457eef577d8
ThanhLT <59819766+thanhltph08395@users.noreply.github.com>
2021-04-14 05:55:29 +0700

    commit

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,2 +1,27 @@
+import { Category } from "./category";
+
 export class Product {
+    id: Number;
+    title: string;
+    price: number;
+    image: string;
+    desc: string;
+    details: string;
+    categoryId: Number;
+
+    category?: Category;
+    created_at?: Date;
+    updated_at?: Date;
+
+    constructor(title: string, id: number, image: string, desc: string, price: number, details: string, category: Array<Category> = []) {
+
+        this.title = title;
+        this.image = image;
+        this.id = id;
+        this.desc = desc;
+        this.price = price;
+        this.details = details;
+        this.categoryId = this.categoryId;
+
+    }
 }
--- src/app/screens/products/prod-list/prod-list.component.ts
+++ src/app/screens/products/prod-list/prod-list.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { Category } from 'src/app/models/category';
+import { BookService } from 'src/app/services/book.service';
+import { Router } from '@angular/router';
+import { ORDER_DATA } from 'src/app/mock-data/ORDER_DATA';
 
 @Component({
   selector: 'app-prod-list',
@@ -7,9 +13,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdListComponent implements OnInit {
 
-  constructor() { }
+  prod: Array<any> = [];
+  totalPage = null;
+  pages: Array<Number> = [];
+
+  orderData: any[] = ORDER_DATA;
+
+  filterObject = {
+    orderBy: "1",
+    keyword: "",
+    pagesize: 10,
+    currentPage: 1
+  }
+  constructor(
+    private route: Router,
+    private bookService: BookService,
+
+  ) { }
 
   ngOnInit(): void {
+    this.getAllProducts();
   }
 
+  getAllProducts() {
+    this.bookService.getAll(this.filterObject).subscribe(res => {
+
+      this.prod = res.data;
+      this.totalPage = res.last_page;
+      if (this.pages.length == 0) {
+        for (let i = 1; i <= this.totalPage; i++) this.pages.push(i)
+      }
+    })
+  }
+  remove(id) {
+    let conf = confirm("Bạn chắc chắn xóa");
+    if (conf) {
+      this.bookService.removeMultiple(id).subscribe(data => {
+        this.getAllProducts();
+        this.route.navigate(['/admin/product-list']);
+      })
+    }
+  }
 }
